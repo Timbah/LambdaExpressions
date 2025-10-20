315fd226ac0fd46f82e8026c9053d0838368d7ae
Thembelani Ngema <thembelaningema20@gmail.com>
2025-10-20 20:29:19 +0200

    Lambda expressions examples

---
+++ src/main/java/expressions/Main.java
@@ -0,0 +1,26 @@
+package expressions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>(List.of(
+                "Alpha", "bravo", "charlie", "delta"));
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        System.out.println("_".repeat(30));
+        list.forEach((myString) -> System.out.println(myString));
+
+        System.out.println("_".repeat(30));
+        list.forEach((var myString) -> {
+            char first = myString.charAt(0);
+            System.out.println(myString + " means " + first);
+        });
+    }
+}
